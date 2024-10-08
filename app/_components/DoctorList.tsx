bae684b0d2137ca774b0f4736873d80c355283a5
DFBlok <s219392862@mandela.ac.za>
2024-10-08 14:19:59 +0200

    added the doctor cards with details

--- app/_components/DoctorList.tsx
+++ app/_components/DoctorList.tsx
@@ -12,7 +12,7 @@ const DoctorLits = ({ doctorList }: DoctorListProps) => {
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7">
         {doctorList &&
           doctorList.map((doctor, index) => (
-            <div className="border-[1px] rounded-lg p-3">
+            <div className="border-[1px] rounded-lg p-3" key={index}>
               <Image
                 src={doctor.attributes?.image?.data?.attributes?.url}
                 alt={doctor}
@@ -20,6 +20,21 @@ const DoctorLits = ({ doctorList }: DoctorListProps) => {
                 height={200}
                 className="h-[200px] w-full object-cover rounded-lg"
               />
+              <div className="mt-3 items-baseline flex flex-col gap-3">
+                <h2 className="text-[10px] bg-blue-100 py-1 rounded-full px-2 text-blue-700">
+                  {doctor.attributes?.categories?.data[0].attributes?.name}
+                </h2>
+                <h2 className="font-bold">{doctor.attributes?.Name}</h2>
+                <h2 className="text-blue-700 text-sm">
+                  {doctor.attributes?.Year_of_Experience}
+                </h2>
+                <h2 className="text-gray-500 text-sm">
+                  {doctor.attributes?.Address}
+                </h2>
+                <h2 className="p-2 px-3 text-blue-600 border-[1px] border-blue-600 rounded-full w-full text-center text-[11px] mt-2 cursor-pointer hover:bg-blue-600 hover:text-white">
+                  Book Now
+                </h2>
+              </div>
             </div>
           ))}
       </div>
