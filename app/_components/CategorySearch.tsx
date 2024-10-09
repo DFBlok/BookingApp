50777695f1f8e726d673ef537c26b9e9e39a0a16
DFBlok <s219392862@mandela.ac.za>
2024-10-09 14:15:05 +0200

    skeleton effect categories

--- app/_components/CategorySearch.tsx
+++ app/_components/CategorySearch.tsx
@@ -54,25 +54,29 @@ const CategorySearch = () => {
       </div>
       {/* Display List of Catergories */}
       <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-        {categoryList.map(
-          (item, index) =>
-            index < 6 && (
-              <div
-                key={index}
-                className="flex flex-col text-center  cursor-pointer gap-2 items-center p-5 bg-blue-50 m-2 rounded-lg hover:scale-110 transition-all ease-in-out"
-              >
-                <Image
-                  src={item.attributes?.Icon?.data.attributes?.url}
-                  alt="icon"
-                  width={40}
-                  height={40}
-                />
-                <label className="text-blue-600 text-sm">
-                  {item?.attributes?.name}
-                </label>
-              </div>
+        {categoryList.length > 0
+          ? categoryList.map(
+              (item, index) =>
+                index < 6 && (
+                  <div
+                    key={index}
+                    className="flex flex-col text-center  cursor-pointer gap-2 items-center p-5 bg-blue-50 m-2 rounded-lg hover:scale-110 transition-all ease-in-out"
+                  >
+                    <Image
+                      src={item.attributes?.Icon?.data.attributes?.url}
+                      alt="icon"
+                      width={40}
+                      height={40}
+                    />
+                    <label className="text-blue-600 text-sm">
+                      {item?.attributes?.name}
+                    </label>
+                  </div>
+                )
             )
-        )}
+          : [1, 2, 3, 4, 5, 6].map((item, index) => (
+              <div className="h-[100px] w-[100px] m-2 bg-slate-200 animate-pulse rounded-lg"></div>
+            ))}
         {/*{!categoryList ? (
           <p>Loading categories...</p> // Show a loading state while fetching
         ) : categoryList.length > 0 ? (
