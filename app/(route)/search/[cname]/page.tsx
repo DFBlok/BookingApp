03639880a036f4b9407cd840ab86f7ca91ebf2be
DFBlok <s219392862@mandela.ac.za>
2024-10-15 15:04:55 +0200

    app routing

---
+++ app/(route)/search/[cname]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useEffect } from "react";
+
+const Search = ({ params }: any) => {
+  useEffect(() => {
+    console.log(params);
+  }, []);
+  return <div>Search </div>;
+};
+
+export default Search;
