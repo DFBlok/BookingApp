cfc142ccaabcda7f45e2b92c2aabd4744fc9e0f5
DFBlok <s219392862@mandela.ac.za>
2024-10-23 10:47:17 +0200

    installed sid menu from shadcn

--- app/(route)/search/_component/CategoryList.tsx
+++ app/(route)/search/_component/CategoryList.tsx
@@ -1,6 +1,17 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import GlobalApi from "@/app/_utils/GlobalApi";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
 
 type CategoryItem = {
   id: number;
@@ -29,7 +40,27 @@ function CategoryList() {
       setCategoryList(resp.data.data);
     });
   };
-  return <div>CategoryList</div>;
+  return (
+    <div className="h-screen mt-5 flex flex-col">
+      <Command>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>Profile</CommandItem>
+            <CommandItem>Billing</CommandItem>
+            <CommandItem>Settings</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </div>
+  );
 }
 
 export default CategoryList;
